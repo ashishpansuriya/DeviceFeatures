803967914b8fd3d16e9442afb84f77405adbbfc8
ashishpansuriya <93606686+ashishpansuriya@users.noreply.github.com>
2022-05-20 21:25:01 +0530

    Home Screen Design

--- navigation/PlaceNavigator.js
+++ navigation/PlaceNavigator.js
@@ -1,21 +1,46 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Platform} from 'react-native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import NewPlaceScreen from '../screens/NewPlaceScreen';
 import MapScreen from '../screens/MapScreen';
 import PlaceScreenDetails from '../screens/PlaceScreenDetails';
 import PlaceListScreen from '../screens/PlaceListScreen';
-
+import {COLORS} from '../constants/Color';
+import { Ionicons } from "@expo/vector-icons";
 const Stack = createNativeStackNavigator();
 const PlaceNavigator = () => {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="NewPlaceScreen" component={NewPlaceScreen} />
+      <Stack.Screen
+        name="NewPlaceScreen"
+        component={NewPlaceScreen}
+        options={({navigation}) => ({
+          title: 'All Products',
+          headerStyle: {
+            backgroundColor:
+              Platform.OS === 'android' ? COLORS.primaryColor : COLORS.White,
+          },
+          headerTintColor:
+            Platform.OS === 'android' ? COLORS.White : COLORS.primaryColor,
+          headerTitleStyle: {
+            fontWeight: '700',
+            fontSize: 20,
+          },
+
+          headerRight: () => (
+            <Ionicons
+              name="cart"
+              size={35}
+              color={Platform.OS === 'android' ? COLORS.Orange : COLORS.Red}
+              onPress={() => navigation.navigate('cartScreen')}
+            />
+          ),
+        })}
+      />
       <Stack.Screen name="MapScreen" component={MapScreen} />
       <Stack.Screen name="PlaceScreenDetails" component={PlaceScreenDetails} />
       <Stack.Screen name="PlaceListScreen" component={PlaceListScreen} />
-
     </Stack.Navigator>
   );
 };
--- screens/NewPlaceScreen.js
+++ screens/NewPlaceScreen.js
@@ -1,13 +1,20 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Button} from 'react-native';
 
-const NewPlaceScreen = () => {
+const NewPlaceScreen = (props) => {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <Text>Changes you make will automatically reload.</Text>
       <Text>Shake your phone to open the developer menu.</Text>
+
+      <Button
+        title="Click"
+        onPress={() => {
+          props.navigation.navigate('MapScreen');
+        }}
+      />
     </View>
   );
 };
